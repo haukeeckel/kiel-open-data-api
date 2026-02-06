56804ca0d9ea34a82003a1bbd71a2ea65f194046
haukeeckel <79708578+haukeeckel@users.noreply.github.com>
2026-02-06 21:01:38 +0100

    fix(repo): default host to 127.0.0.1

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export type NodeEnv = (typeof NODE_ENVS)[number];
 
 export const DEFAULT_NODE_ENV: NodeEnv = 'development';
 export const DEFAULT_PORT = 3000;
-export const DEFAULT_HOST = '0.0.0.0';
+export const DEFAULT_HOST = '127.0.0.1';
