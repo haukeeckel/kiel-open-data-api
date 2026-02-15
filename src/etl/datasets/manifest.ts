18b2f3da8591b4fc6bc6bcf519dde8afc519f2ad
Hauke Eckel <h.eckel@great-value-group.com>
2026-02-15 14:58:03 +0100

    feat(etl): add subdistricts age groups dataset

--- src/etl/datasets/manifest.ts
+++ src/etl/datasets/manifest.ts
@@ -12,6 +12,7 @@ import { DISTRICTS_POPULATION } from './districts_population.js';
 import { DISTRICTS_RELIGION } from './districts_religion.js';
 import { DISTRICTS_UNEMPLOYED_COUNT } from './districts_unemployed_count.js';
 import { DISTRICTS_UNEMPLOYED_RATE } from './districts_unemployed_rate.js';
+import { SUBDISTRICTS_AGE_GROUPS } from './subdistricts_age_groups.js';
 import { SUBDISTRICTS_POPULATION } from './subdistricts_population.js';
 
 import type { DatasetConfig } from './types.js';
@@ -32,6 +33,7 @@ export const DATASET_MANIFEST: readonly DatasetConfig[] = [
   DISTRICTS_UNEMPLOYED_RATE,
   DISTRICTS_RELIGION,
   SUBDISTRICTS_POPULATION,
+  SUBDISTRICTS_AGE_GROUPS,
 ];
 
 export function validateDatasetManifest(datasets: readonly DatasetConfig[]): void {
